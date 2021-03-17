b078e5f0989811f17d44cc192fdc51b10f6f3e45
Akiomi Kamakura <akiomik@gmail.com>
2021-03-17 22:41:50 +0900

    Add test script

---
+++ libs/logue-sdk-stub/platform/minilogue-xd/inc/utils/float_math.h
@@ -0,0 +1,19 @@
+// Copyright 2021 Akiomi Kamakura
+
+#ifndef LIBS_LOGUE_SDK_STUB_PLATFORM_MINILOGUE_XD_INC_UTILS_FLOAT_MATH_H_
+
+#define LIBS_LOGUE_SDK_STUB_PLATFORM_MINILOGUE_XD_INC_UTILS_FLOAT_MATH_H_
+
+#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <math.h>
+
+float clipmaxf(const float x, const float m) {
+  return std::min(x, m);
+}
+
+float fastersinf(const float x) {
+  return sinf(x);
+}
+
+#endif  // LIBS_LOGUE_SDK_STUB_PLATFORM_MINILOGUE_XD_INC_UTILS_FLOAT_MATH_H_
---
+++ tools/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+ROOT_DIR=${SCRIPT_DIR}/..
+PROJECT_DIRS=(
+  $ROOT_DIR/osc/pulse/platform/minilogue-xd
+)
+
+for project_dir in ${PROJECT_DIRS[@]}; do
+  echo Test $project_dir
+  cd $project_dir
+  export MAKEFILES=../../test.mk
+  make cleantest && make test
+done
