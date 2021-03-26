0a4661cf32ed505e10296cd37a44f83408f8b64f
Akiomi Kamakura <akiomik@gmail.com>
2021-03-26 20:33:33 +0900

    Revert tools/build.sh

---
+++ tools/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+ROOT_DIR=${SCRIPT_DIR}/..
+PROJECT_DIRS=(
+  $ROOT_DIR/osc/pulse/platform/minilogue-xd
+  $ROOT_DIR/osc/noise/platform/minilogue-xd
+  $ROOT_DIR/osc/pokegold/platform/minilogue-xd
+)
+
+stat=0
+
+for project_dir in ${PROJECT_DIRS[@]}; do
+  echo Build $project_dir
+  cd $project_dir
+  make && make check
+  stat=$(($stat | $?))
+done
+
+exit $stat
